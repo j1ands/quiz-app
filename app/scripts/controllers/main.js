26698880d06f55bbcecbd5572639c45d681c3a83
j1ands <jlandau@uoregon.edu>
2014-12-09 22:41:26 -0500

    working score

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -20,31 +20,53 @@ angular.module('quizApp')
 		      "q": "Who is the best ping pong player at FSA?", 
 		      'options': [{ 'value': "Mike"} , { 'value': "Eddie"} , {'value' : "Nimit"} , { 'value': "Patrick"}],
 		      'answer': "Nimit" ,
-		      'difficulty': 1
+		      'difficulty': 1,
+		      'tried': false
 		    },
 		    { "q": "Which robot name was chanted during Lego Mindstorms?", 
 		      'options':[{ 'value': 'infiniteLoop'} , { 'value': 'noHope.js'} , {'value' : 'johnny5'} , { 'value': 'none of the above'}], 
 		      'answer':'noHope.js',
-		      'difficulty': 5
+		      'difficulty': 5,
+		      'tried': false
 		    },
 		    { 
 		      'q': "Out of the following frontend frameworks, which framework is most rails-like", 
 		      'options':[{ 'value': 'Ember.js'} ,{ 'value': 'Angular.js'} , {'value' : 'Backbone.js'} , { 'value': 'Meteor.js'}], 
 		      'answer':'Ember.js',
-		      'difficulty': 3
+		      'difficulty': 3,
+		      'tried': false
 		    },
 		    { 
 		      'q': "Which project used a local data store?", 
 		      'options':[{ 'value': 'TripPlanner'} ,{ 'value': 'Twitter.js'} , {'value' : 'WikiWalker'} , { 'value': 'WikiStack'}], 
 		      'answer':'Twitter.js',
-		      'difficulty': 2
+		      'difficulty': 2,
+		      'tried': false
 		    }
 	  	];
 	$scope.rating = 'difficulty';
-	$scope.selected = "button";
-	$scope.radioSelected = false;
-	$scope.radioSelect = function()
+	$scope.active = false;
+	$scope.score = 0;
+	$scope.checkAnswer = function(question)
 	{
-		$scope.radioSelected = true;
-	};
+		if(!question.tried)
+		{		
+			if(question.selected == question.answer)
+			{
+				$scope.score += 10;
+			}
+			else
+			{
+				$scope.score -= 10;
+			}
+		}
+		question.tried = true;
+	}
+	// $scope.showSubmit = function()
+	// {
+	// 	console.log($scope.question);
+	// 	$scope.question.selected = true;
+	// }
+	//$scope.selected = "button";
+
   });
